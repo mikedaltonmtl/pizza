db3c7522881bbad77bc48bf80a472364e341631b
miledaltonmtl <michael.j.dalton@gmail.com>
2023-05-03 15:13:48 -0400

    codealong finished #8

---
+++ GameObject.js
@@ -0,0 +1,55 @@
+class GameObject {
+  constructor(config) {
+    this.id = null;
+    this.isMounted = false;
+    this.x = config.x || 0;
+    this.y = config.y || 0;
+    this.direction = config.direction || 'down';
+    this.sprite = new Sprite({
+      gameObject: this,
+      src: config.src || './images/characters/people/hero.png',
+    });
+
+    this.behaviorLoop = config.behaviorLoop || [];
+    this.behaviorLoopIndex = 0;
+  };
+
+  mount(map) {
+    this.isMounted = true;
+    map.addWall(this.x, this.y);
+
+    // If we have a behaviour, kick off after a short delay
+    setTimeout(() => {
+      this.doBehaviorEvent(map);
+    }, 10)
+  }
+
+  update() {
+  };
+
+  async doBehaviorEvent(map) {
+
+    // Do nothing during cut scene or if no behavior defined
+    if (map.isCutscenePlaying || this.behaviorLoop.length === 0) {
+      return;
+    }
+
+    // Setting up our event with the relevant info
+    let eventConfig = this.behaviorLoop[this.behaviorLoopIndex];
+    eventConfig.who = this.id;
+
+    // Create an event instance out of our next event config
+    const eventHandler = new OverworldEvent({ map, event: eventConfig });
+    await eventHandler.init();
+
+    // Setting the next event to fire
+    this.behaviorLoopIndex += 1;
+    if (this.behaviorLoopIndex === this.behaviorLoop.length) {
+      this.behaviorLoopIndex = 0;
+    }
+
+    // Do it again!
+    this.doBehaviorEvent(map);
+  };
+
+}
---
+++ Overworld.js
@@ -0,0 +1,67 @@
+class Overworld {
+  constructor(config) {
+    this.element = config.element;
+    this.canvas = this.element.querySelector('.game-canvas');
+    this.ctx = this.canvas.getContext('2d');
+    this.map = null;
+  }
+
+  startGameLoop() {
+    const step = () => {
+
+      // Clear the canvas before redrawing images
+      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+      // Establish the camera person
+      const cameraPerson = this.map.gameObjects.hero;
+
+      // Update the objects before drawing them
+      Object.values(this.map.gameObjects).forEach(object => {
+        object.update({
+          arrow: this.directionInput.direction,
+          map: this.map
+        })
+      });
+
+      // Draw lower level
+      this.map.drawLowerImage(this.ctx, cameraPerson);
+
+      // Draw game objects
+      Object.values(this.map.gameObjects).sort((a,b) => {
+        return a.y - b.y;
+      }).forEach(object => {
+        object.sprite.draw(this.ctx, cameraPerson);
+      });
+
+      // Draw upper level
+      this.map.drawUpperImage(this.ctx, cameraPerson);
+
+
+      // requestanimationFrame call the function every frame - avoids step calling step in an infinite loop
+      requestAnimationFrame(() => {
+        step();
+      })
+    }
+    step();
+  }
+
+  init() {
+    this.map = new OverworldMap(window.OverworldMaps.DemoRoom);
+    this.map.mountObjects();
+
+    this.directionInput = new DirectionInput();
+    this.directionInput.init();
+    this.directionInput.direction; // ex 'down' or undefined
+
+    this.startGameLoop();
+
+    this.map.startCutscene([
+      { who: 'hero', type: 'walk', direction: 'down' },
+      { who: 'hero', type: 'walk', direction: 'down' },
+      { who: 'npcA', type: 'walk', direction: 'left' },
+      { who: 'npcA', type: 'walk', direction: 'left' },
+      { who: 'npcA', type: 'stand', direction: 'up', time: 800 }
+    ])
+  }
+
+}
