124c3b8a2c3fa0dc5bae3b77a4359181ec716ea0
miledaltonmtl <michael.j.dalton@gmail.com>
2023-05-04 12:43:02 -0400

    fix problem with 'stand' by adding event handler

--- OverworldEvent.js
+++ OverworldEvent.js
@@ -1,5 +1,5 @@
 class OverworldEvent {
-  constructor({ map, event }) {
+  constructor({ map, event}) {
     this.map = map;
     this.event = event;
   }
@@ -8,49 +8,47 @@ class OverworldEvent {
     const who = this.map.gameObjects[ this.event.who ];
     who.startBehavior({
       map: this.map
-    },
-    {
-      type: 'stand',
+    }, {
+      type: "stand",
       direction: this.event.direction,
       time: this.event.time
     })
-
-    // Set up a handler to complete when person is done walking & resolve the event
-    // const completeHandler = e => {
-    //   if (e.detail.whoId === this.event.who) {
-    //     document.removeEventListener('PersonWalkingComplete', completeHandler);
-    //     resolve();
-    //   }
-    // }
-
-    // document.addEventListener('PersonWalkingComplete', completeHandler);
+    
+    //Set up a handler to complete when correct person is done walking, then resolve the event
+    const completeHandler = e => {
+      if (e.detail.whoId === this.event.who) {
+        document.removeEventListener("PersonStandComplete", completeHandler);
+        resolve();
+      }
+    }
+    document.addEventListener("PersonStandComplete", completeHandler)
   }
 
   walk(resolve) {
     const who = this.map.gameObjects[ this.event.who ];
     who.startBehavior({
       map: this.map
-    },
-    {
-      type: 'walk',
+    }, {
+      type: "walk",
       direction: this.event.direction,
       retry: true
     })
 
-    // Set up a handler to complete when person is done walking & resolve the event
+    //Set up a handler to complete when correct person is done walking, then resolve the event
     const completeHandler = e => {
       if (e.detail.whoId === this.event.who) {
-        document.removeEventListener('PersonWalkingComplete', completeHandler);
+        document.removeEventListener("PersonWalkingComplete", completeHandler);
         resolve();
       }
     }
-    document.addEventListener('PersonWalkingComplete', completeHandler);
+    document.addEventListener("PersonWalkingComplete", completeHandler)
+
   }
 
   init() {
     return new Promise(resolve => {
-      this[this.event.type](resolve);
-    });
+      this[this.event.type](resolve)      
+    })
   }
 
-};
+}
